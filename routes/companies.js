65f50bd451bfa11e4204c76e55c86a97b542ed59
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2022-09-03 17:19:02 +0200

    feat: a new route for the company registration

--- routes/companies.js
+++ routes/companies.js
@@ -10,4 +10,18 @@ router.get('/', async function(req, res, next) {
   return res.json(companies);
 });
 
+/* Create a new company */
+router.post('/', async function(req, res, next) {
+  const transaction = await db.sequelize.transaction()
+  try {
+    const { name } = req.body
+    const createdCompany = await db.Company.create({ name }, { transaction })
+    await transaction.commit()
+    return res.json(createdCompany)
+  } catch (error) {
+    await transaction.rollback()
+    return res.status(400).send(error)
+  }
+})
+
 module.exports = router;
